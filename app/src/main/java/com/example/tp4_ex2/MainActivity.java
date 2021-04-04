41adcb9cea5259e70fb14bb5c96a92b8489e9638
Geo <geoffrey.privard@gmail.com>
2021-04-05 00:01:47 +0200

    real push commit

--- app/src/main/java/com/example/tp4_ex2/MainActivity.java
+++ app/src/main/java/com/example/tp4_ex2/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.tp4_ex2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnCard).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragment = getSupportFragmentManager();
+                SimpleDialogFragment simpleDialogFragment = SimpleDialogFragment.newInstance(R.layout.card);
+                simpleDialogFragment.show(fragment, "fragment_simple_dialog");
+            }
+        });
+
+        findViewById(R.id.btnPaypal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragment = getSupportFragmentManager();
+                SimpleDialogFragment simpleDialogFragment = SimpleDialogFragment.newInstance(R.layout.paypal);
+                simpleDialogFragment.show(fragment, "fragment_simple_dialog");
+            }
+        });
     }
 }
--- app/src/main/java/com/example/tp4_ex2/SimpleDialogFragment.java
+++ app/src/main/java/com/example/tp4_ex2/SimpleDialogFragment.java
@@ -1,64 +1,58 @@
 package com.example.tp4_ex2;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SimpleDialogFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class SimpleDialogFragment extends Fragment {
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class SimpleDialogFragment extends DialogFragment {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
+    private EditText mEditText;
+    private Button btn;
+
 
     public SimpleDialogFragment() {
-        // Required empty public constructor
+        // le fragment est créé par la méthode newInstance
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SimpleDialogFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SimpleDialogFragment newInstance(String param1, String param2) {
-        SimpleDialogFragment fragment = new SimpleDialogFragment();
+    public static SimpleDialogFragment newInstance(int layoutID) {
+
+        SimpleDialogFragment frag = new SimpleDialogFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        args.putInt("layoutID", layoutID);
+        frag.setArguments(args);
+        return frag;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
+
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_simple_dialog, container, false);
+        int layoutID = getArguments().getInt("layoutID", R.layout.card);
+
+        return inflater.inflate(layoutID, container);
+
     }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
+        super.onViewCreated(view, savedInstanceState);
+
+        getDialog().getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE
+        );
+
+    }
+
+
 }
